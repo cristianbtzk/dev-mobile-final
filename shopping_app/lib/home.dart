94124b840b09cf5bf5ddf1177ecbe6225b9b09a6
Cristian Butzke <cristian.btzk@gmail.com>
2023-12-12 21:44:14 -0300

    Add navigation bar

---
+++ shopping_app/lib/home.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Colors.blue.shade400,
+      ),
+      drawer: Drawer(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildHeader(context),
+              buildMenuItems(context),
+            ],
+          ),
+        ),
+      ),
+      /* body: SafeArea(
+        child: Column(),
+      ), */
+    );
+  }
+}
+
+Widget buildHeader(BuildContext context) => Material(
+      color: Colors.blue.shade400,
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 24 + MediaQuery.of(context).padding.top,
+            bottom: 24,
+          ),
+          child: const Column(
+            children: [
+              CircleAvatar(
+                radius: 52,
+                backgroundImage: NetworkImage(
+                    'https://img.freepik.com/vetores-premium/perfil-de-avatar-ilustracoes-vetoriais-para-site-redes-sociais-icone-de-perfil-de-usuario_495897-230.jpg?w=2000'),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'User',
+                style: TextStyle(fontSize: 28, color: Colors.white),
+              ),
+              Text(
+                'user@email.com',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+Widget buildMenuItems(BuildContext context) => Container(
+      padding: const EdgeInsets.all(12),
+      child: Wrap(
+        runSpacing: 2,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, '/exercises');
+            },
+          ),
+          const Divider(color: Colors.black),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Categories'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
